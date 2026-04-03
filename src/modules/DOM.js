857662fae0e75a8d7aa9975a4bab3ad35578ae28
Saief <A.S.Bokor@outlook.com>
2026-04-03 18:58:07 +0100

    add some code into many different files, mainly some HTML boilerplate additions and css styling aswell as the initial DOM elements within the DOM.js file

--- src/modules/DOM.js
+++ src/modules/DOM.js
@@ -1,2 +1,9 @@
 // DOM.js
 // Handles DOM manipulation
+
+//Imports
+
+// ----------------------------------------------------------
+// Collect DOM Elements
+const weatherData = document.querySelector(".weatherData");
+const searchInput = document.querySelector(".searchInput");
