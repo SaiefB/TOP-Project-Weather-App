9a457906a1acff804a27167dfb29a00540e82afb
Saief <A.S.Bokor@outlook.com>
2026-04-03 21:00:32 +0100

    add a function to api.js to fetch data from the weather API

--- src/modules/api.js
+++ src/modules/api.js
@@ -2,3 +2,33 @@
 // Handles all fetch calls to the weather API
 
 // function to fetch weather data
+async function fetchWeatherData() {
+  fetch(
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london,uk?unitGroup=uk&contentType=json&key=2HU4R9TR5QED9H8S3DP86YZB6",
+    {
+      method: "GET",
+      headers: {},
+    },
+  )
+    .then((response) => {
+      if (!response.ok) {
+        return response.text().then((text) => {
+          throw new Error(text);
+        });
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Weather data for London, UK:", data);
+    })
+    .catch((error) => {
+      console.error("Error fetching weather data:", error);
+    });
+}
+
+async function fetchWeatherData() {
+  const response = await fetch(
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london,uk?unitGroup=uk&contentType=json&key=2HU4R9TR5QED9H8S3DP86YZB6",
+  );
+  response.json();
+}
